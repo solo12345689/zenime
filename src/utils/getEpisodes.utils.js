07f353232d9791451859ba0b241fe006b4ee673a
solo12345689 <sarthak.hasija.10095@kiitworld.in>
2026-07-11 12:31:58 +0530

    fix: fetch all episode pages concurrently on watch page

--- src/utils/getEpisodes.utils.js
+++ src/utils/getEpisodes.utils.js
@@ -4,10 +4,30 @@ export default async function getEpisodes(slug, postId) {
   const api_url = import.meta.env.VITE_API_URL;
   try {
     const response = await axios.get(
-      `${api_url}/anime/${slug}/episodes?postId=${postId}`
+      `${api_url}/anime/${slug}/episodes?postId=${postId}&page=1`
     );
     const results = response.data;
-    const mappedEpisodes = (results.episodes || []).map((ep) => ({
+    let episodesList = results.episodes || [];
+    const maxPages = results.max_pages || 1;
+
+    if (maxPages > 1) {
+      const promises = [];
+      for (let p = 2; p <= maxPages; p++) {
+        promises.push(
+          axios.get(
+            `${api_url}/anime/${slug}/episodes?postId=${postId}&page=${p}`
+          )
+        );
+      }
+      const pageResponses = await Promise.all(promises);
+      pageResponses.forEach((res) => {
+        if (res.data && res.data.episodes) {
+          episodesList = episodesList.concat(res.data.episodes);
+        }
+      });
+    }
+
+    const mappedEpisodes = episodesList.map((ep) => ({
       id: `ep=${ep.number}`,
       episode_no: ep.number,
       title: ep.title || `Episode ${ep.number}`,
@@ -15,6 +35,7 @@ export default async function getEpisodes(slug, postId) {
       released: ep.released,
       slug: ep.slug,
     }));
+
     return {
       episodes: mappedEpisodes,
       totalEpisodes: mappedEpisodes.length,
